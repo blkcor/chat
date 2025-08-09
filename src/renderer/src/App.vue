50412c870840bb9b830e56a67f8443a7781d8d04
blkcor <blkcor.dev@gmail.com>
2025-08-09 18:07:11 +0800

    chore: init project

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-red-200">Hello Electron</h1>
+    <button @click="ipcHandle">Send IPC</button>
+  </div>
+</template>
+
